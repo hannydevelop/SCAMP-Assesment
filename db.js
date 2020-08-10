be09a60188287ff67a0a1453e820cace033547f9
hannydevelop <ukpaiugochiibem@gmail.com>
2020-08-10 18:33:41 +0100

    fixing depreciation warning from Server Discovery and Monitoring engine

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/SCAMP', (err) =>{
+mongoose.connect('mongodb://localhost:27017/SCAMP', { useNewUrlParser: true, useUnifiedTopology: true },(err) =>{
     if (!err)
        console.log('mongodb connected successfully');
     else
